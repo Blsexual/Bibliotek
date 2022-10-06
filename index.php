e3c6fb26259eb87ef908623df51399973399955d
Blsexual <kevin.johansson@elev.ga.ntig.se>
2022-10-06 11:48:11 +0200

    Kan logga in och registrera dig

---
+++ index.php
@@ -0,0 +1,212 @@
+<?php
+    session_start();
+    require_once('db.php');
+    $_SESSION['PersonNum'] = NULL;
+    $_SESSION['Pass'] = NULL;
+    $Fel = 1;
+?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Bibliotek</title>
+        <link rel="stylesheet" href="index.css">
+    </head>
+    <body>
+
+        <?php
+            if (isset($_POST['PersonNum'])){
+                $sql = "SELECT Namn,`Password`,Personnummer FROM anvandare";
+                $result = $conn->query($sql);
+                if ($result->num_rows > 0) {
+                    while($row = $result->fetch_assoc()) {
+                        if ((isset($_POST['PersonNum'])) && ($_POST['PersonNum'] == $row['Personnummer']) && ($_POST['Pass'] == $row['Password'])){
+                            $_SESSION['PersonNum'] = $row['Personnummer'];
+                            $_SESSION['Pass'] = $row['Password'];
+                            $Fel = 0;
+                            Header('Location:anvandare.php');
+                        }
+                    }
+                }
+            if ((isset($_POST['Fel']) == 1) && ($Fel == 1)){
+                $_SESSION['Fel'] = 1;
+                Header('Location:index.php');
+            }
+            }
+        ?>
+
+        <?php
+            if ($_SESSION['Fel'] === 1){
+                echo "Fel";
+            }
+            if (isset($_POST['Fel']) == 0){
+                $_SESSION['Fel'] = 0;
+            }
+        ?>
+
+        <form method='post'>
+            <input type='hidden' name='Fel' value='1'>
+            Personnummner:<input type='text' name='PersonNum' required placeholder='YYYYMMDDXXXX' minlength='12' maxlength='12'><br>
+            Lösenord:<input type='password' name='Pass' required><br>
+            <input type='submit' value='Logga in'>
+        </form>
+        <form method = 'post' action='registrera.php'>
+            <input type='submit' value='Registrera dig!'>
+        </form>
+
+        
+
+        
+        
+        
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    </body>
+</html>
---
+++ registrera.php
@@ -0,0 +1,124 @@
+<?php
+    session_start();
+    require_once('db.php');
+    $_SESSION['PersonNum'] = NULL;
+    $_SESSION['Pass'] = NULL;
+    print_r($_POST);
+    $Fel = 1;
+?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Bibliotek</title>
+        <link rel="stylesheet" href="index.css">
+    </head>
+    <body>
+        <?php
+            if(@$_SESSION['PerFel'] == 1){
+                echo "Personnumret finns redan registrerat";
+                $_SESSION['PerFel'] = 0;
+            }
+            if(@$_SESSION['PassFel'] == 1){
+                echo "Lösenorden stämmer inte";
+                $_SESSION['PassFel'] = 0;
+            }
+        ?>
+        <?php
+            if (isset($_POST)){
+                $sql = "SELECT Personnummer FROM anvandare";
+                $result = $conn->query($sql);
+                if ($result->num_rows > 0) {
+                    while($row = $result->fetch_assoc()) {
+                        if (@$_POST['PersonNum'] == $row['Personnummer']){
+                            $_SESSION['PerFel'] = 1;  
+                            Header('Location:registrera.php');
+                        }
+                    }
+                }
+                if ((@$_POST['Pass1'] != @$_POST['Pass2']) && ($_SESSION['PerFel'] != 1)){
+                    $_SESSION['PassFel'] = 1;  
+                    Header('Location:registrera.php');
+                }
+                if (($_SESSION['PerFel'] != 1) && ($_SESSION['PassFel'] != 1) && (@$_POST['Fel'] == 1)){
+                    $sql = $conn->prepare("INSERT INTO anvandare (Namn,`Password`,Personnummer) VALUES (?,?,?)");
+                    $sql->bind_param("sss", $Namn,$Pass,$Pers);
+
+                    $Namn = $_POST['Namn'];
+                    $Pass = $_POST['Pass1'];
+                    $Pers = $_POST['PersonNum'];
+
+                    $sql->execute();
+                    $sql->close();
+                    
+                    $_SESSION['PersonNum'] = $Pers;
+                    $_SESSION['Pass'] = $Pass;
+                    Header('Location:anvandare.php');
+                }
+            }
+        ?>
+        <form method='post'>
+            <input type='hidden' name='Fel' value='1'>
+            Namn:<input type='text' name='Namn' required><br>
+            Personnummner:<input type='text' name='PersonNum' required placeholder='YYYYMMDDXXXX' minlength='12' maxlength='12'><br>
+            Lösenord:<input type='password' name='Pass1' required minlength='6' maxlength='30'><br>
+            Verifiera lösenord:<input type='password' name='Pass2' required minlength='6' maxlength='30'><br>
+            <input type='submit' value='Registrera'>
+        </form>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    </body>
+</html>
