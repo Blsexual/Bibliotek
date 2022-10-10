63f3091d92cce097584a3e6caa54ff0699cd53b8
Blsexual <kevin.johansson@elev.ga.ntig.se>
2022-10-10 12:02:12 +0200

    kan lämna in bokker

--- anvandare.php
+++ anvandare.php
@@ -25,9 +25,10 @@
                         while($row = $result->fetch_assoc()) {
                             if ((@$_SESSION['PersonNum'] == $row['Personnummer']) && (@$_SESSION['Pass'] == $row['Password'])){
                                 echo "Inloggad som " . $row['Namn'];
-                                if ($row['Admin'] == 1){
-                                    Header('Location:admin.php');
-                                }
+                                $AnvID = $row['ID'];
+                                // if ($row['Admin'] == 1){
+                                //     Header('Location:admin.php');
+                                // }
                             }
                         }
                     }
@@ -63,27 +64,86 @@
             <?php
                 if ($_POST['Tab'] == "Användare"){ // Användare
                     echo "<div id='TabAnvändare' class='Tab'>";
-                        echo " <br>";
-                        $sql = "SELECT Namn FROM anvandare";
+                        $sql = "SELECT DISTINCT lan.EID AS EID FROM lan INNER JOIN exemplar ON lan.AID = $AnvID AND Inlamnad = 0";
                         $result = $conn->query($sql);
                         if ($result->num_rows > 0) {
                             while($row = $result->fetch_assoc()) {
-                                echo "<br>" . $row['Namn'];
+                                $EID = $row['EID'];
+                                echo $row['EID'] . " ";
+                                $sql2 = "SELECT bok.Namn FROM bok INNER JOIN exemplar ON bok.ISBN = exemplar.ISBN AND exemplar.ID = $EID";
+                                $result2 = $conn->query($sql2);
+                                if ($result2->num_rows > 0) {
+                                    while($row2 = $result2->fetch_assoc()) {
+                                        echo $row2['Namn'];
+                                        echo "<form method='post' action='lamnain.php'>";
+                                            echo "<input type='hidden' name='EID', value='$EID'>";
+                                            echo "<input type='submit' value='Lämna in'>";
+                                        echo "</form>";
+                                        echo "<br>";
+
+                                    }
+                                }
                             }
                         }
                     echo "</div>";
                 }
-
                 if ($_POST['Tab'] == "Bok"){ // Bok
                     echo "<div id='TabBok' class='Tab'>";
-                        echo " <br>";
-                        $sql = "SELECT Namn FROM bok";
-                        $result = $conn->query($sql);
-                        if ($result->num_rows > 0) {
-                            while($row = $result->fetch_assoc()) {
-                                echo "<br>" . $row['Namn'];
+                        echo "<form method='post'>";
+                            echo "<input type='hidden' name='Tab', value='Bok'>";
+                            echo "<input type='text' name='VisaBok'>";
+                            echo "<input type='submit' value='Sök'>";
+                        echo "</form>";
+                        if (!isset($_POST['VisaBok'])){
+                            $_POST['VisaBok'] = "";
+                        }
+                        if (isset($_POST['VisaBok'])){
+                            $sql = "SELECT DISTINCT bok.Namn,bok.ISBN FROM bok INNER JOIN exemplar ON bok.Namn LIKE ? AND bok.ISBN = exemplar.ISBN";
+                            $stmt = $conn->prepare($sql); 
+                            $stmt->bind_param("s", $Namn);
+                            $Namn = $_POST['VisaBok'];
+                            $Namn = "%".$Namn."%";
+                            $stmt->execute();
+                            $result = $stmt->get_result();
+                            while ($row = $result->fetch_assoc()) {
+                                $Num = 0;
+                                $ISBN = $row['ISBN'];
+                                echo "<br>" . $row['Namn'] . "<br> ----------- <br>";
+
+                                $sql2 = "SELECT bok.Namn AS Namn,exemplar.ID FROM `exemplar` INNER JOIN `bok` ON $ISBN = exemplar.ISBN AND $ISBN = bok.ISBN ORDER BY `exemplar`.`ID` ASC";
+                                $result2 = $conn->query($sql2);
+                                if ($result2->num_rows > 0) {
+                                    while($row2 = $result2->fetch_assoc()) {
+                                        $Num += 1;
+                                        echo $Num . " " . $row2['Namn'];
+                                        $EID = $row2['ID'];
+
+                                        $sql3 = "SELECT lan.Inlamnad FROM `lan`,`exemplar` WHERE $EID = lan.EID ORDER BY lan.Inlamnad ASC;";
+                                        $result3 = $conn->query($sql3);
+                                        if ($result3->num_rows > 0) {
+                                            while($row3 = $result3->fetch_assoc()) {
+                                                if ($row3['Inlamnad'] != 1){
+                                                    echo "<button>Utlånad</button><br>";
+                                                    break;
+                                                }
+                                                else{
+                                                    echo "<button>Låna</button><br>";
+                                                    break;
+                                                }
+                                            }
+                                        }
+                                        else{
+                                            echo "<button>Låna</button><br>";
+                                        }
+                                    }
+                                }
+                                else{
+                                    echo "Inga exemplar <br>";
+                                }
                             }
                         }
+
+
                     echo "</div>";
                 }
 
@@ -103,12 +163,57 @@
 
                 if ($_POST['Tab'] == "Film"){ // Film
                     echo "<div id='TabFilm' class='Tab'>";
-                        echo " <br>";
-                        $sql = "SELECT Titel FROM film";
-                        $result = $conn->query($sql);
-                        if ($result->num_rows > 0) {
-                            while($row = $result->fetch_assoc()) {
-                                echo "<br>" . $row['Titel'];
+                        echo "<form method='post'>";
+                            echo "<input type='hidden' name='Tab', value='Film'>";
+                            echo "<input type='text' name='VisaFilm'>";
+                            echo "<input type='submit' value='Sök'>";
+                        echo "</form>";
+                        if (!isset($_POST['VisaFilm'])){
+                            $_POST['VisaFilm'] = "";
+                        }
+                        if (isset($_POST['VisaFilm'])){
+                            $sql = "SELECT DISTINCT film.Titel,film.ID FROM film INNER JOIN exemplar ON film.Titel LIKE ? AND film.ID = exemplar.FID";
+                            $stmt = $conn->prepare($sql); 
+                            $stmt->bind_param("s", $Titel);
+                            $Titel = $_POST['VisaFilm'];
+                            $Titel = "%".$Titel."%";
+                            $stmt->execute();
+                            $result = $stmt->get_result();
+                            while ($row = $result->fetch_assoc()) {
+                                $Num = 0;
+                                $ID = $row['ID'];
+                                echo "<br><br>" . $row['Titel'] . "<br> ----------- <br>";
+
+                                $sql2 = "SELECT film.Titel AS Titel,exemplar.ID FROM `exemplar` INNER JOIN `film` ON $ID = exemplar.FID AND $ID = film.ID ORDER BY `exemplar`.`ID` ASC";
+                                $result2 = $conn->query($sql2);
+                                if ($result2->num_rows > 0) {
+                                    while($row2 = $result2->fetch_assoc()) {
+                                        $Num += 1;
+                                        echo $Num . " " . $row2['Titel'];
+                                        $EID = $row2['ID'];
+
+                                        $sql3 = "SELECT lan.Inlamnad FROM `lan`,`exemplar` WHERE $EID = lan.EID ORDER BY lan.Inlamnad ASC;";
+                                        $result3 = $conn->query($sql3);
+                                        if ($result3->num_rows > 0) {
+                                            while($row3 = $result3->fetch_assoc()) {
+                                                if ($row3['Inlamnad'] != 1){
+                                                    echo "<button>Utlånad</button><br>";
+                                                    break;
+                                                }
+                                                else{
+                                                    echo "<button>Låna</button><br>";
+                                                    break;
+                                                }
+                                            }
+                                        }
+                                        else{
+                                            echo "<button>Låna</button><br>";
+                                        }
+                                    }
+                                }
+                                else{
+                                    echo "Inga exemplar <br>";
+                                }
                             }
                         }
                     echo "</div>";
